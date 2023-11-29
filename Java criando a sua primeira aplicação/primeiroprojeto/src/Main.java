3abefd60c94c8ef92a4942a31ad6d65c98f2fa67
FMTSL <Felipe_Matos@hotmail.com.br>
2023-11-28 21:02:50 -0300

    Primeiro código java alura

---
+++ Java criando a sua primeira aplicação/primeiroprojeto/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo! Seja bem vindo Felipe!");
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}
